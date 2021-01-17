84409fb160e1c98218915f315b04f338ac24156d
Adetunji Sunmonu <asunmonu@chandler.com.au>
2021-01-18 02:30:15 +1000

    restructure the solution

---
+++ test/ConferenceAPITest/Services/SessionServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using ConferenceApi.Interfaces;
+using ConferenceApi.Services;
+using Xunit;
+
+namespace ConferenceAPITest.Services
+{
+    public class SessionServiceTest
+    {
+        [Fact]
+        public void SessionService_GetAllSessions()
+        {
+            ISessionService sessionService = new SessionService();
+
+            Assert.Equal(2, sessionService.GetSessions().Count);
+        }
+
+        [Fact]
+        public void SessionService_GetAllSessionsForSpecificSpeakerAndTimeSlot()
+        {
+            ISessionService sessionService = new SessionService();
+            var name = "Tim Davis";
+            var date = "21/12/2020";
+            var timeSlot = "15:00 - 16:00";
+            Assert.Single(sessionService.GetSessionsFor(name, date, timeSlot).Result);
+        }
+
+        [Fact]
+        public void SessionService_EmptyWhenAllCriteriaDidNotMatch()
+        {
+            ISessionService sessionService = new SessionService();
+            var name = "Tim Davis";
+            var date = "21/12/2020";
+            var timeSlot = "11:00 - 12:00";
+            Assert.Empty(sessionService.GetSessionsFor(name, date, timeSlot).Result);
+        }
+    }
+}
