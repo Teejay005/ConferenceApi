cc151329b64aa14c050dacbb2772aac27f9bbb65
Adetunji Sunmonu <asunmonu@chandler.com.au>
2021-01-17 23:47:18 +1000

    added swagger configuration for docs

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ConferenceApi
 {
@@ -25,22 +26,85 @@ namespace ConferenceApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             services.AddControllers();
+            services.AddLogging();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BOQ Conference API", Version = "v1" });
+                c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+                {
+                    Description = "ApiKey must appear in header",
+                    Type = SecuritySchemeType.ApiKey,
+                    Name = "X-API-KEY",
+                    In = ParameterLocation.Header,
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                      new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "ApiKey"
+                            },
+                        },
+                        new List<string>()
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Use(async (context, next) =>
+            {
+                context.Response.OnStarting(() =>
+                {
+                    if (context.Response.Headers.ContainsKey("Cache-Control"))
+                    {
+                        context.Response.Headers["Cache-Control"] = "no-cache,no-store";
+                    }
+                    else
+                    {
+                        context.Response.Headers.Add("Cache-Control", "no-cache,no-store");
+                    }
+                    if (context.Response.Headers.ContainsKey("Pragma"))
+                    {
+                        context.Response.Headers["Pragma"] = "no-cache";
+                    }
+                    else
+                    {
+                        context.Response.Headers.Add("Pragma", "no-cache");
+                    }
+                    return Task.FromResult(0);
+                });
+                await next.Invoke();
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+           
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Heritage SMS API");
+            });
 
             app.UseEndpoints(endpoints =>
             {
